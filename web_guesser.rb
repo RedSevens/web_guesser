9909571d72cc5be01fea98df8985519b000a9bcc
Christopher Roman <xredsevens@gmail.com>
2013-07-09 16:32:29 -0400

    Finally got the game to work

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 #Creates a random number from 1-99
-number = rand(99) + 1
+NUMBER = (rand(99) + 1).to_s
 
 get '/' do
-	erb :index, :locals => {:number => number}
+	your_guess = params['guess']
+	message = check(your_guess)
+	erb :index, :locals => {:number => $number, :message => message, :answer => @answer}
+end
+
+def check(guess)
+	number = NUMBER
+	if guess == nil
+		return nil
+	elsif guess.to_i - number.to_i > 5
+		return "Way too high"
+	elsif guess.to_i > number.to_i
+		return "Too high"
+	elsif number.to_i - guess.to_i > 5
+		return "Way too low"
+	elsif guess.to_i < number.to_i
+		return "Too low"
+	else
+		@answer = true
+		return "You got it right!"
+	end
 end
