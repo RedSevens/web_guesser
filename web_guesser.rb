8b74cf384f637600729d55a51a39aed03fd4f46d
Christopher Roman <xredsevens@gmail.com>
2013-07-09 16:51:49 -0400

    End of 7/9/13(Tuesday). Must complete the counter for remaining chances.

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,15 +3,17 @@ require 'sinatra/reloader'
 
 #Creates a random number from 1-99
 NUMBER = (rand(99) + 1).to_s
+$chances ||= 5
 
 get '/' do
 	your_guess = params['guess']
 	message = check(your_guess)
-	erb :index, :locals => {:number => $number, :message => message, :answer => @answer}
+	erb :index, :locals => {:number => NUMBER, :message => message, :answer => @answer, :chances => $chances}
 end
 
 def check(guess)
 	number = NUMBER
+	@answer ||= false
 	if guess == nil
 		return nil
 	elsif guess.to_i - number.to_i > 5
@@ -25,5 +27,8 @@ def check(guess)
 	else
 		@answer = true
 		return "You got it right!"
+	end
+	if @answer == false
+		$chances -= 1
 	end
 end
