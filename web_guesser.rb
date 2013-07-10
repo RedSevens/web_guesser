e3687d47b1601c715f564eb4b47ee9e4ac6fd654
Christopher Roman <xredsevens@gmail.com>
2013-07-10 12:12:56 -0400

    Centered Webpage and cleaned up code so there are less global variables

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-$number = (rand(99) + 1).to_s
+number = (rand(99) + 1).to_s
 $chances = 9
-$previous_guess = 0
+previous_guess = 0
 
 get '/' do
-	if $chances > 0 && $previous_guess != params['guess'] && $answer == false
+	if $chances > 0 && previous_guess != params['guess'] && $answer == false && params['guess'] != ""
 		$chances -= 1
 	elsif $chances < 1 || $answer == true
 		$answer = true
 		$chances = 8
-		$number = (rand(99) + 1).to_s
+		number = (rand(99) + 1).to_s
 	end
 	your_guess = params['guess']
-	message = check(your_guess)
-	$previous_guess = your_guess
-	erb :index, :locals => {:number => $number, :message => message, :answer => $answer, :chances => $chances}
+	message = check(your_guess, number)
+	previous_guess = your_guess
+	erb :index, :locals => {:number => number, :message => message, :answer => $answer, :chances => $chances}
 end
 
-def check(guess)
-	number = $number
+def check(guess, number)
 	$answer = false
 	if guess == nil
 		return nil
